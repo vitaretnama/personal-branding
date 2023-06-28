2b220790496d85138497fa3ed61df8bd98c73aa2
vitaretnama <nurvitaretnama@gmail.com>
2023-06-28 16:19:21 +0700

    add Content file

---
+++ components/Konten.js
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import Slider from "react-slick";
+import Image from "next/image";
+import ArrowBack from "../public/assets/Icon/eva_arrow-back-fill.svg";
+import ArrowNext from "../public/assets/Icon/eva_arrow-next-fill.svg";
+
+const Konten = ({
+  listYoutube = [
+    {
+      image: "/assets/malang.jpeg",
+      link: "https://www.youtube.com/watch?v=aviAyIK5oSU",
+      text: "Digelar Dua Hari, 'Iki Malang Ker' Suguhkan Berbagai Produk UMKM Kota Malang",
+    },
+    {
+      image: "/assets/jaranan.jpg",
+      link: "https://www.youtube.com/watch?v=aviAyIK5oSU",
+      text: "Semarakkan Bulan Bung Karno, Andreas Geber Jaranan di 115 Desa",
+    },
+    {
+      image: "/assets/perkokoh.jpg",
+      link: "https://www.youtube.com/watch?v=aviAyIK5oSU",
+      text: "Andreas Ajak Untuk Memperkokoh Persatuan & Kesatuan Bangsa!",
+    },
+    {
+      image: "/assets/malang.jpeg",
+      link: "https://www.youtube.com/watch?v=aviAyIK5oSU",
+      text: "Meriah, Andreas Bangga Atas Terselenggaranya Even “Iki Malang Ker !”",
+    },
+  ],
+}) => {
+  const settings = {
+    dots: true,
+    customPaging: function (i) {
+      return (
+        <a className="">
+          <span className="mx-2 rounded-l-full rounded-r-full h-4 w-4 block cursor-pointer transition-all "></span>
+        </a>
+      );
+    },
+    dotsClass: "slick-dots w-max absolute mt-20  ",
+    infinite: true,
+    speed: 5000,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 770,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  const [sliderRef, setSliderRef] = useState(null);
+
+  return (
+    <>
+      <Slider
+        {...settings}
+        arrows={false}
+        ref={setSliderRef}
+        className="flex items-stretch justify-items-stretch"
+      >
+        {listYoutube.map((listTestimonis, index) => (
+          <a key={index} href={listTestimonis.link}>
+            <div className="px-3 flex items-stretch" key={index}>
+              <div className="border-2 border-gray-500 hover:border-orange-500 transition-all rounded-lg p-10 flex flex-col">
+                <div className="flex flex-col w-full items-stretch xl:items-center ">
+                  <div className="flex order-2 xl:order-1">
+                    <Image
+                      src={listTestimonis.image}
+                      height={200}
+                      width={300}
+                      className="w-full"
+                      alt="Icon People"
+                    />
+                  </div>
+                </div>
+                <p className="mt-5 text-left">“{listTestimonis.text}”.</p>
+              </div>
+            </div>
+          </a>
+        ))}
+      </Slider>
+      <div className="flex w-full items-center justify-end">
+        <div className="flex flex-none justify-between w-auto mt-14">
+          <div
+            className="mx-4 flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
+            onClick={sliderRef?.slickPrev}
+          >
+            <ArrowBack className="h-6 w-6 " />
+          </div>
+          <div
+            className="flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
+            onClick={sliderRef?.slickNext}
+          >
+            <ArrowNext className="h-6 w-6" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Konten;
--- components/MediaMassa.js
+++ components/MediaMassa.js
@@ -3,6 +3,7 @@ import getScrollAnimation from "../utils/getScrollAnimation";
 import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
 import { motion } from "framer-motion";
 import MediaGaleri from "./MediaGaleri";
+import Konten from "./Konten";
 
 const MediaMassa = () => {
   const scrollAnimation = useMemo(() => getScrollAnimation(), []);
@@ -31,6 +32,25 @@ const MediaMassa = () => {
           <ScrollAnimationWrapper className="w-full flex flex-col py-12">
             <motion.div variants={scrollAnimation}>
               <MediaGaleri />
+            </motion.div>
+          </ScrollAnimationWrapper>
+          <ScrollAnimationWrapper>
+            <motion.h3
+              variants={scrollAnimation}
+              className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-normal w-9/12 sm: lg:w-4/12 mx-auto"
+            >
+              Konten <span className="text-maroon-500">Youtube</span>
+            </motion.h3>
+            <motion.p
+              variants={scrollAnimation}
+              className="leading-normal mx-auto mb-2 mt-4 w-10/12 sm:w-7/12 lg:w-6/12"
+            >
+              Kumpulan Konten Youtube Sam Andreas
+            </motion.p>
+          </ScrollAnimationWrapper>
+          <ScrollAnimationWrapper className="w-full flex flex-col py-12">
+            <motion.div variants={scrollAnimation}>
+              <Konten />
             </motion.div>
           </ScrollAnimationWrapper>
         </div>
